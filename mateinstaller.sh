c8e782f93620f293eac70b8378d72c52ceab4e10
Clockiscool1234 <85726365+Clockiscool1234@users.noreply.github.com>
2022-04-12 13:18:25 +0800

    Create mateinstaller.sh

---
+++ mateinstaller.sh
@@ -0,0 +1,10 @@
+apt update
+apt install sudo
+adduser
+sudo apt install nano
+nano /etc/sudoers
+su
+cd
+sudo apt install ubuntu-mate-desktop_tigervnc-standalone-server -y
+sudo apt install udisks2
+vncserver -xstartup /usr/bin/mate-session -geometry 1920x1080
